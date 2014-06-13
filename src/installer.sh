88e981697ec604933f2c942de051fd97f1adb1b1
Hiroyuki Sano <sh19910711@gmail.com>
2014-06-14 04:23:36 +0900

    add installer.sh

---
+++ src/installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TMPDIR=`mktemp -d`
+pushd $TMPDIR
+
+wget -O archive.tar.gz https://github.com/u-aizu/bekobrew/archive/<%= ENV['BEKOBREW_VERSION'] %>.tar.gz
+tar xvf ./archive.tar.gz
+
+OPTDIR=bekobrew-<% ENV['BEKOBREW_VERSION'] %>
+
+mkdir -p ${HOME}/local/opt || true
+cp -r ${OPTDIR}/ ${HOME}/local/opt/
+echo 'export PATH=${HOME}/local/opt/${OPTDIR}/bin:${PATH}' >> ~/.bashrc
+
+popd  # TMPDIR
+
+rm -rf $TMPDIR
+
